def213a2848685b1ae0ac6d0a6a6520137b36c13
G6EJD <d.l.bird@sky.com>
2018-04-20 14:54:09 +0100

    Update ESP8266_DHT_Three_Sensors.ino

--- ESP8266_DHT_Three_Sensors.ino
+++ ESP8266_DHT_Three_Sensors.ino
@@ -2,31 +2,42 @@
 // (c) D L Bird 2018
 
 #include "DHT.h"   //https://github.com/adafruit/DHT-sensor-library 
-// Create the DHT temperature and humidity sensor object
+// Create the DHT temperature and humidity sensor objects
+// The example here is for a Wemos D1 Mini, you can chose and use any (available) pins you like for each sensor 
 DHT sensor1(D4, DHT11); // Change the pin D4 to suit your needs and sensor type to match yours e.g. DHT11, or DHT 21 or DHT22
 DHT sensor2(D3, DHT11); // Change the pin D3 to suit your needs and sensor type to match yours e.g. DHT11, or DHT 21 or DHT22
 DHT sensor3(D2, DHT11); // Change the pin D2 to suit your needs and sensor type to match yours e.g. DHT11, or DHT 21 or DHT22
-//DHT sensor4(D1, DHT11); // Change the pin D1 to suit your needs and sensor type to match yours e.g. DHT11, or DHT 21 or DHT22
+DHT sensor4(D1, DHT11); // Change the pin D1 to suit your needs and sensor type to match yours e.g. DHT11, or DHT 21 or DHT22
 
-float sensor1_temp, sensor2_temp, sensor3_temp, sensor4_temp; // Add more if required
-float sensor1_humi, sensor2_humi, sensor3_humi, sensor4_humi; // Add more if required
+// Now declare the variables that will hold the sensor readings
+float sensor1_temp,
+      sensor2_temp,
+      sensor3_temp,
+      sensor4_temp; // Add more if required
+
+float sensor1_humi,
+      sensor2_humi,
+      sensor3_humi,
+      sensor4_humi; // Add more if required
 
 void setup(void) {
   Serial.begin(9600);
   sensor1.begin();
   sensor2.begin();
   sensor3.begin();
-  //sensor4.begin(); // Uncomment for 4 sensors
+  sensor4.begin(); // Add more if you have the spare pins available!
 }
 
 void loop() {
-  // Read DHT temperature and humidity values
+  // Read DHT temperature and humidity values for Sensor-1
   sensor1_temp = sensor1.readTemperature();
   sensor1_humi = sensor1.readHumidity();
 
+  // Read DHT temperature and humidity values for Sensor-2
   sensor2_temp = sensor2.readTemperature();
   sensor2_humi = sensor2.readHumidity();
 
+  // Read DHT temperature and humidity values for Sensor-3
   sensor3_temp = sensor3.readTemperature();
   sensor3_humi = sensor3.readHumidity();
 
@@ -52,7 +63,5 @@ void loop() {
   //Serial.print(sensor4_temp,1); Serial.print(String(char(176))+"C  "); // Uncomment for 4 sensors
   //Serial.print(sensor4_humi,1); Serial.println("%RH"); // Uncomment for 4 sensors
   //Serial.println();
-
-delay(3000);
+  delay(3000);
 }
-
